fcfe53044cf65d6adb54e45638bdb6a8a73a25a2
Vikalp2003 <vikalpshinde25@gmail.com>
2023-06-07 02:31:37 +0530

    made UI of Onboarding Screen

--- jobhub/jobhub/lib/views/ui/onboarding/onboarding_screen.dart
+++ jobhub/jobhub/lib/views/ui/onboarding/onboarding_screen.dart
@@ -1,16 +1,101 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:jobhub/controllers/exports.dart';
 import 'package:jobhub/views/common/exports.dart';
+import 'package:jobhub/views/ui/onboarding/widgets/page_one.dart';
+import 'package:jobhub/views/ui/onboarding/widgets/page_three.dart';
+import 'package:jobhub/views/ui/onboarding/widgets/page_two.dart';
+import 'package:provider/provider.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
-
-class OnBoardingScreen extends StatelessWidget {
+class OnBoardingScreen extends StatefulWidget {
   const OnBoardingScreen({super.key});
 
   @override
+  State<OnBoardingScreen> createState() => _OnBoardingScreenState();
+}
+
+class _OnBoardingScreenState extends State<OnBoardingScreen> {
+  final PageController _pageController = PageController();
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: ReusableText(text: "OnBoardingScreen", style: appstyle(30, Color(kDark.value), FontWeight.bold),),
-      ),
-    );
+    return Scaffold(body:
+        Consumer<OnBoardNotifier>(builder: (context, OnBoardNotifier, child) {
+      return Stack(
+        children: [
+          PageView(
+            physics: OnBoardNotifier.isLastPage
+                ? const NeverScrollableScrollPhysics()
+                : const AlwaysScrollableScrollPhysics(),
+            controller: _pageController,
+            onPageChanged: (page) {
+              OnBoardNotifier.isLastPage = page == 2;
+            },
+            children: [
+              PageOne(),
+              PageTwo(),
+              PageThree(),
+            ],
+          ),
+          Positioned(
+              bottom: hieght * 0.09,
+              left: 0,
+              right: 0,
+              child: Center(
+                child: OnBoardNotifier.isLastPage
+                    ? SizedBox.shrink()
+                    : SmoothPageIndicator(
+                        controller: _pageController,
+                        count: 3,
+                        effect: JumpingDotEffect(
+                          dotHeight: 12,
+                          dotWidth: 12,
+                          spacing: 10,
+                          dotColor: Color(kDarkGrey.value).withOpacity(0.5),
+                          activeDotColor: Color(kLight.value),
+                          verticalOffset: 30,
+                        ),
+                      ),
+              )),
+          Positioned(
+              child: OnBoardNotifier.isLastPage ? const SizedBox.shrink() : Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: EdgeInsets.symmetric(vertical: 30.h, horizontal: 20.0.w),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      _pageController.jumpToPage(2);
+                    },
+                    child: ReusableText(
+                        text: "Skip",
+                        style:
+                            appstyle(16, Color(kLight.value), FontWeight.w500)),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      _pageController.nextPage(
+                          duration: Duration(milliseconds: 300),
+                          curve: Curves.ease);
+                    },
+                    child: ReusableText(
+                        text: "Next",
+                        style:
+                            appstyle(16, Color(kLight.value), FontWeight.w500)),
+                  )
+                ],
+              ),
+            ),
+          )),
+        ],
+      );
+    }));
   }
 }
--- jobhub/jobhub/lib/views/ui/onboarding/widgets/page_three.dart
+++ jobhub/jobhub/lib/views/ui/onboarding/widgets/page_three.dart
@@ -1,10 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:jobhub/views/common/custom_outline_btn.dart';
+import 'package:jobhub/views/common/exports.dart';
+import 'package:jobhub/views/common/height_spacer.dart';
+
+import '../../../../constants/app_constants.dart';
 
 class PageThree extends StatelessWidget {
   const PageThree({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+     return Scaffold(
+      body: Container(
+        width: width,
+        height: hieght,
+        color: Color(kLightBlue.value),
+        child: Column(
+          children: [
+            Image.asset("assets/images/page3.png"),
+            HeightSpacer(size: 20),
+
+            ReusableText(text: "Welcome to JobHub", style: appstyle(30, Color(kLight.value), FontWeight.w600)),
+
+            HeightSpacer(size: 20),
+
+            Padding(padding: EdgeInsets.symmetric   (horizontal: 30.w),
+              child: Text(
+                "We help you find your dream job to your skillset, location and preference to build your career",
+                textAlign: TextAlign.center,
+                style: appstyle(14, Color(kLight.value), FontWeight.normal),
+              ),
+            ),
+            HeightSpacer(size: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                CustomOutlineBtn(
+                  onTap: () {},
+                  text: "Login",
+                  width: width*0.4,
+                  height: hieght*0.06,
+                  color: Color(kLight.value), color2: null,
+                  
+                ),
+
+                GestureDetector(
+                  onTap: null,
+                  child: Container(
+                    width: width*0.4,
+                    height: hieght*0.06,
+                    color: Color(kLight.value),
+                    child: Center(child: ReusableText(text: "Sign Up", style: appstyle(16, Color(kLightBlue.value), FontWeight.w600),)),
+                  ),
+                )
+              ],
+            ),
+
+            HeightSpacer(size: 30),
+            ReusableText(text: "Continue as guest", style: appstyle(16, Color(kLight.value), FontWeight.w400))
+
+          ],
+        ),
+      ),
+    );
   }
 }
