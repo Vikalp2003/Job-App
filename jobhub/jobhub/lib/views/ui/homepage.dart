65032d49096156f7005d2576ac8e0c9b0eb3faea
Vikalp2003 <vikalpshinde25@gmail.com>
2023-06-15 22:42:04 +0530

    updates some login after files

--- jobhub/jobhub/lib/views/ui/homepage.dart
+++ jobhub/jobhub/lib/views/ui/homepage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:jobhub/views/common/app_bar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,6 +14,17 @@ class _HomePageState extends State<HomePage> {
   
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: PreferredSize(preferredSize: Size.fromHeight(50.h),
+        child: CustomAppBar(
+          actions: [
+            Padding(padding: EdgeInsets.all(12.h)),
+          ], child: const CircleAvatar(
+            radius: 15,
+            backgroundImage: AssetImage("assets/images/user.png"),
+          ),
+          ),
+      ),
+    );
   }
 }
